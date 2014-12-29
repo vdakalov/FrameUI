b17930e32211fadaca80699a1d1d72d00f6aafe8
Viktor Dakalov <atlantidu@gmail.com>
2014-12-30 03:11:12 +0500

    Implement VerticalPanel

--- lib/src/common/panels/VerticalPanel.dart
+++ lib/src/common/panels/VerticalPanel.dart
@@ -2,4 +2,26 @@ part of FrameUI;
 
 class VerticalPanel extends Panel {
 
+  @override
+  render(CanvasRenderingContext2D context) {
+
+    super.render(context);
+
+    int offset = 0;
+
+    elements
+    .where((element){ return element.style.visible; })
+    .forEach((element){
+      element.area = new Rectangle(
+          area.left + style.paddingLeft,
+          area.top + style.paddingTop + offset,
+          element.area.width,
+          element.area.height);
+
+      offset += element.area.height + 2;
+      element.render(context);
+    });
+
+  }
+
 }
--- web/main.dart
+++ web/main.dart
@@ -5,16 +5,15 @@ import "package:FrameUI/init.dart" as FrameUI;
 
 main() {
 
-  int width = 500, height = 500;
+  int width = 800, height = 600;
 
   FrameUI.Frame main = new FrameUI.Frame(width, height);
   main.mount(document.body);
 
+  // TOP PANEL
   FrameUI.HorizontalPanel controlbar = new FrameUI.HorizontalPanel();
   main.panels.add(controlbar);
-
   controlbar.area = new Rectangle(0, 0, width, 24);
-
 
   FrameUI.IconButton add =
       new FrameUI.IconButton(name: "Add", action: (){print("press add");});
@@ -25,5 +24,10 @@ main() {
       new FrameUI.IconButton(name: "Save", action: (){print("press save");});
   controlbar.elements.add(save);
   save.style.cursor = FrameUI.CURSOR.POINTER;
+
+  // RIGHT PANEL
+  FrameUI.VerticalPanel models = new FrameUI.VerticalPanel();
+  main.panels.add(models);
+  models.area = new Rectangle(width - 250, controlbar.area.height, 250, 300);
 
 }
